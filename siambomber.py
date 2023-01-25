c012d7e754834483fa2016f67fe1d79c27ffd351
SIAM RAHMAN <118335510+SIAMRAHMAN000@users.noreply.github.com>
2023-01-25 10:32:41 +0100

    Update siambomber.py

--- siambomber.py
+++ siambomber.py
@@ -1,3 +1,17 @@
 import os
+import time
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+try:
+    import requests
+except:
+    printchar(cyanBold + "installing requests.....", 0.05)
+    time.sleep(2)
+    os.system("pip install requests")
+    import requests
+    printchar(greenBold + "requests successfully installed.....", 0.05)
+    time.sleep(2)
+    clr()
 os.chdir('.main')
 os.system('python main.py')
